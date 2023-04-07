3ebea3c59919a4048a374ee92af80cef2fd82b0c
Çağrı KART <ckarte43@gmail.com>
2023-04-07 15:48:39 +0300

    todo app bitti

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { TodoLayer } from "./context/TodoContext";
+import reducer, { initialState } from "./context/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </TodoLayer>
   </React.StrictMode>
 );
 
